1ab98a6c248a65ad6738f51b06c834faa4978ac7
Michele <m.giulietti@teamdev.it>
2023-10-24 12:00:57 +0200

    added dap ui

--- lua/michele/packer.lua
+++ lua/michele/packer.lua
@@ -4,55 +4,59 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-	-- Packer can manage itself
-	use 'wbthomason/packer.nvim'
-	if packer_bootstrap then
-		require('packer').sync()
-	end
-	use {
-		'nvim-telescope/telescope.nvim', tag = '0.1.4',
-		-- or                            , branch = '0.1.x',
-		requires = { {'nvim-lua/plenary.nvim'} }
-	}
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+  if packer_bootstrap then
+    require('packer').sync()
+  end
+  use {
+    'nvim-telescope/telescope.nvim', tag = '0.1.4',
+    -- or                            , branch = '0.1.x',
+    requires = { { 'nvim-lua/plenary.nvim' } }
+  }
 
-	use({ 'rose-pine/neovim', as = 'rose-pine', config = function()
-		vim.cmd('colorscheme rose-pine')
-	end
-})
-use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
-use('nvim-treesitter/playground')
-use('ThePrimeagen/harpoon')
-use 'mbbill/undotree'
-use('tpope/vim-fugitive')
-use('f-person/git-blame.nvim')
--- LSP --
-use {
-	'VonHeikemen/lsp-zero.nvim',
-	requires = {
-		--- Uncomment these if you want to manage LSP servers from neovim
-		{'williamboman/mason.nvim'},
-		{'williamboman/mason-lspconfig.nvim'},
+  use({
+    'rose-pine/neovim',
+    as = 'rose-pine',
+    config = function()
+      vim.cmd('colorscheme rose-pine')
+    end
+  })
+  use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
+  use('nvim-treesitter/playground')
+  use('ThePrimeagen/harpoon')
+  use 'mbbill/undotree'
+  use('tpope/vim-fugitive')
+  use('f-person/git-blame.nvim')
+  -- LSP --
+  use {
+    'VonHeikemen/lsp-zero.nvim',
+    requires = {
+      --- Uncomment these if you want to manage LSP servers from neovim
+      { 'williamboman/mason.nvim' },
+      { 'williamboman/mason-lspconfig.nvim' },
 
-		-- LSP Support
-		{'neovim/nvim-lspconfig'},
-		-- Autocompletion
-		{'hrsh7th/nvim-cmp'},
-		{'hrsh7th/cmp-nvim-lsp'},
-		{'L3MON4D3/LuaSnip'},
-	}
-}
+      -- LSP Support
+      { 'neovim/nvim-lspconfig' },
+      -- Autocompletion
+      { 'hrsh7th/nvim-cmp' },
+      { 'hrsh7th/cmp-nvim-lsp' },
+      { 'L3MON4D3/LuaSnip' },
+    }
+  }
 
--- LSP Flutter --
-use {
+  -- LSP Flutter --
+  use {
     'akinsho/flutter-tools.nvim',
     requires = {
-        'nvim-lua/plenary.nvim',
-        'stevearc/dressing.nvim', -- optional for vim.ui.select
+      'nvim-lua/plenary.nvim',
+      'stevearc/dressing.nvim',   -- optional for vim.ui.select
     },
-}
+  }
 
-use('dart-lang/dart-vim-plugin')
-use('thosakwe/vim-flutter')
-use 'mfussenegger/nvim-dap'
-use 'https://github.com/adelarsq/image_preview.nvim'
+  use('dart-lang/dart-vim-plugin')
+  use('thosakwe/vim-flutter')
+  use 'mfussenegger/nvim-dap'
+  use { "rcarriga/nvim-dap-ui", requires = { "mfussenegger/nvim-dap" } }
+  use 'https://github.com/adelarsq/image_preview.nvim'
 end)
