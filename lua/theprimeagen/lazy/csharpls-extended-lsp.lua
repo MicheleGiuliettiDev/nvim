2d52916b7895090aa0eaa9c97ee9f27ad791f402
Michele Giulietti <michele_giulietti@yahoo.it>
2025-05-08 20:17:37 +0200

    fix csharp

--- lua/theprimeagen/lazy/csharpls-extended-lsp.lua
+++ lua/theprimeagen/lazy/csharpls-extended-lsp.lua
@@ -7,22 +7,7 @@ return {
   config = function()
     local lspconfig = require("lspconfig")
 
-    lspconfig.csharp_ls.setup({
-      handlers = {
-        ["textDocument/definition"] = require("csharpls_extended").handler,
-        ["textDocument/typeDefinition"] = require("csharpls_extended").handler,
-      },
-      -- Optional: Add custom keymaps for enhanced navigation
-      on_attach = function(client, bufnr)
-        vim.keymap.set("n", "gd", function()
-          require("csharpls_extended").lsp_definitions()
-        end, { noremap = true, desc = "Go to extended definition", buffer = bufnr })
-      end,
-    })
-
-    -- For Neovim 0.11+
-    if vim.fn.has("nvim-0.11") == 1 then
-      require("csharpls_extended").buf_read_cmd_bind()
-    end
+    require 'lspconfig'.csharp_ls.setup(lspconfig)
+    require("csharpls_extended").buf_read_cmd_bind()
   end,
 }
