28ebf9b17e73bfa736abddd1d3a87b0498a12ed2
Michele Giulietti <michele_giulietti@yahoo.it>
2025-11-18 18:19:49 +0100

    clangd lsp and set tab space

--- lua/theprimeagen/lazy/lsp.lua
+++ lua/theprimeagen/lazy/lsp.lua
@@ -1,173 +1,175 @@
 local root_files = {
-  '.luarc.json',
-  '.luarc.jsonc',
-  '.luacheckrc',
-  '.stylua.toml',
-  'stylua.toml',
-  'selene.toml',
-  'selene.yml',
-  '.git',
+	".luarc.json",
+	".luarc.jsonc",
+	".luacheckrc",
+	".stylua.toml",
+	"stylua.toml",
+	"selene.toml",
+	"selene.yml",
+	".git",
 }
 
 return {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-        "stevearc/conform.nvim",
-        "williamboman/mason.nvim",
-        "williamboman/mason-lspconfig.nvim",
-        "hrsh7th/cmp-nvim-lsp",
-        "hrsh7th/cmp-buffer",
-        "hrsh7th/cmp-path",
-        "hrsh7th/cmp-cmdline",
-        "hrsh7th/nvim-cmp",
-        "L3MON4D3/LuaSnip",
-        "saadparwaiz1/cmp_luasnip",
-        "j-hui/fidget.nvim",
-    },
+	"neovim/nvim-lspconfig",
+	dependencies = {
+		"stevearc/conform.nvim",
+		"williamboman/mason.nvim",
+		"williamboman/mason-lspconfig.nvim",
+		"hrsh7th/cmp-nvim-lsp",
+		"hrsh7th/cmp-buffer",
+		"hrsh7th/cmp-path",
+		"hrsh7th/cmp-cmdline",
+		"hrsh7th/nvim-cmp",
+		"L3MON4D3/LuaSnip",
+		"saadparwaiz1/cmp_luasnip",
+		"j-hui/fidget.nvim",
+	},
 
-    config = function()
-        local cmp = require('cmp')
-        local cmp_lsp = require("cmp_nvim_lsp")
-        local capabilities = vim.tbl_deep_extend(
-            "force",
-            {},
-            vim.lsp.protocol.make_client_capabilities(),
-            cmp_lsp.default_capabilities())
+	config = function()
+		local cmp = require("cmp")
+		local cmp_lsp = require("cmp_nvim_lsp")
+		local capabilities = vim.tbl_deep_extend(
+			"force",
+			{},
+			vim.lsp.protocol.make_client_capabilities(),
+			cmp_lsp.default_capabilities()
+		)
 
-        require("fidget").setup({})
-        require("mason").setup()
-        require("mason-lspconfig").setup({
-            ensure_installed = {
-                "lua_ls",
-                "rust_analyzer",
-                "gopls",
-            },
-            handlers = {
-                function(server_name) -- default handler (optional)
-                    local lspConfig = vim.lsp.config
-                    lspConfig[server_name].setup {
-                        capabilities = capabilities
-                    }
-                end,
+		require("fidget").setup({})
+		require("mason").setup()
+		require("mason-lspconfig").setup({
+			ensure_installed = {
+				"lua_ls",
+				"rust_analyzer",
+				"gopls",
+			},
+			handlers = {
+				function(server_name) -- default handler (optional)
+					local lspConfig = vim.lsp.config
+					lspConfig[server_name].setup({
+						capabilities = capabilities,
+					})
+				end,
 
-                zls = function()
-                    local lspconfig = vim.lsp.config
-                    lspconfig.zls.setup({
-                        root_dir = lspconfig.util.root_pattern(".git", "build.zig", "zls.json"),
-                        settings = {
-                            zls = {
-                                enable_inlay_hints = true,
-                                enable_snippets = true,
-                                warn_style = true,
-                            },
-                        },
-                    })
-                    vim.g.zig_fmt_parse_errors = 0
-                    vim.g.zig_fmt_autosave = 0
+				zls = function()
+					local lspconfig = vim.lsp.config
+					lspconfig.zls.setup({
+						root_dir = lspconfig.util.root_pattern(".git", "build.zig", "zls.json"),
+						settings = {
+							zls = {
+								enable_inlay_hints = true,
+								enable_snippets = true,
+								warn_style = true,
+							},
+						},
+					})
+					vim.g.zig_fmt_parse_errors = 0
+					vim.g.zig_fmt_autosave = 0
+				end,
+				["lua_ls"] = function()
+					local lspconfig = vim.lsp.config
+					lspconfig.lua_ls.setup({
+						capabilities = capabilities,
+						settings = {
+							Lua = {
+								format = {
+									enable = true,
+									-- Put format options here
+									-- NOTE: the value should be STRING!!
+									defaultConfig = {
+										indent_style = "space",
+										indent_size = "2",
+									},
+								},
+							},
+						},
+					})
+				end,
+			},
+		})
 
-                end,
-                ["lua_ls"] = function()
-                    local lspconfig = vim.lsp.config
-                    lspconfig.lua_ls.setup {
-                        capabilities = capabilities,
-                        settings = {
-                            Lua = {
-                                format = {
-                                    enable = true,
-                                    -- Put format options here
-                                    -- NOTE: the value should be STRING!!
-                                    defaultConfig = {
-                                        indent_style = "space",
-                                        indent_size = "2",
-                                    }
-                                },
-                            }
-                        }
-                    }
-                end,
+		-- lsp config
+		vim.lsp.enable("csharp_ls")
 
-                            }
-        })
+		vim.lsp.enable("angularls")
 
-        -- lsp config
-        vim.lsp.enable('csharp_ls')
+		vim.lsp.enable("copilot")
 
-        vim.lsp.enable('angularls')
+		vim.lsp.enable("docker_language_server")
 
-        vim.lsp.enable('copilot')
+		vim.lsp.enable("helm_ls")
 
-        vim.lsp.enable('docker_language_server')
+		vim.lsp.enable("dartls")
 
-        vim.lsp.enable('helm_ls')
+		vim.lsp.enable("tailwindcss")
 
-        vim.lsp.enable('dartls')
+		vim.lsp.enable("ts_ls")
 
-        vim.lsp.enable('tailwindcss')
+		vim.lsp.enable("vue_ls")
 
-        vim.lsp.enable('ts_ls')
+		vim.lsp.enable("clangd")
 
-        vim.lsp.enable('vue_ls')
+		-- html
+		local capabilities = vim.lsp.protocol.make_client_capabilities()
+		capabilities.textDocument.completion.completionItem.snippetSupport = true
 
+		vim.lsp.config("html", {
+			capabilities = capabilities,
+		})
 
-        -- html
-        local capabilities = vim.lsp.protocol.make_client_capabilities()
-            capabilities.textDocument.completion.completionItem.snippetSupport = true
+		vim.lsp.enable("html")
 
-        vim.lsp.config('html', {
-            capabilities = capabilities,
-        })
+		-- eslint
+		local base_on_attach = vim.lsp.config.eslint.on_attach
+		vim.lsp.config("eslint", {
+			on_attach = function(client, bufnr)
+				if not base_on_attach then
+					return
+				end
 
-        vim.lsp.enable('html')
+				base_on_attach(client, bufnr)
+				vim.api.nvim_create_autocmd("BufWritePre", {
+					buffer = bufnr,
+					command = "LspEslintFixAll",
+				})
+			end,
+		})
 
-      -- eslint
-        local base_on_attach = vim.lsp.config.eslint.on_attach
-        vim.lsp.config("eslint", {
-            on_attach = function(client, bufnr)
-            if not base_on_attach then return end
+		vim.lsp.enable("eslint")
 
-            base_on_attach(client, bufnr)
-            vim.api.nvim_create_autocmd("BufWritePre", {
-            buffer = bufnr,
-            command = "LspEslintFixAll",
-        })
-        end,
-        })
+		local cmp_select = { behavior = cmp.SelectBehavior.Select }
 
-        vim.lsp.enable('eslint')
+		cmp.setup({
+			snippet = {
+				expand = function(args)
+					require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
+				end,
+			},
+			mapping = cmp.mapping.preset.insert({
+				["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
+				["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
+				["<C-y>"] = cmp.mapping.confirm({ select = true }),
+				["<C-Space>"] = cmp.mapping.complete(),
+			}),
+			sources = cmp.config.sources({
+				{ name = "copilot", group_index = 2 },
+				{ name = "nvim_lsp" },
+				{ name = "luasnip" }, -- For luasnip users.
+			}, {
+				{ name = "buffer" },
+			}),
+		})
 
-        local cmp_select = { behavior = cmp.SelectBehavior.Select }
-
-        cmp.setup({
-            snippet = {
-                expand = function(args)
-                    require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-                end,
-            },
-            mapping = cmp.mapping.preset.insert({
-                ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-                ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-                ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-                ["<C-Space>"] = cmp.mapping.complete(),
-            }),
-            sources = cmp.config.sources({
-                { name = "copilot", group_index = 2 },
-                { name = 'nvim_lsp' },
-                { name = 'luasnip' }, -- For luasnip users.
-            }, {
-                { name = 'buffer' },
-            })
-        })
-
-        vim.diagnostic.config({
-            -- update_in_insert = true,
-            float = {
-                focusable = false,
-                style = "minimal",
-                border = "rounded",
-                source = "always",
-                header = "",
-                prefix = "",
-            },
-        })
-    end
+		vim.diagnostic.config({
+			-- update_in_insert = true,
+			float = {
+				focusable = false,
+				style = "minimal",
+				border = "rounded",
+				source = "always",
+				header = "",
+				prefix = "",
+			},
+		})
+	end,
 }
